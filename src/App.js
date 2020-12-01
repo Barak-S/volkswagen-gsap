b083a3baa699fc16c82391fb8a3e469ec794541e
Barak-S <barak.saidoff@gmail.com>
2020-12-01 10:38:06 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import React, { useRef, useEffect, useState } from "react";
+import Menu from './images/menu.svg';
+import Bus from './images/bus1.png';
 import './App.css';
+import { gsap, TweenMax, TweenLite, Power2,TimelineMax }  from 'gsap';
+
 
 function App() {
+
+  let hero = useRef(null)
+  let slider = useRef(null)
+  let logo = useRef(null)
+  let hamburger = useRef(null)
+  let headline = useRef(null)
+
+  useEffect(()=>{     
+    console.log(hero,slider,logo, hamburger, headline)
+    TweenMax.fromTo(hero, 1, {height: "0%"}, {height: "80%", ease: Power2.easeOut })
+    TweenMax.fromTo(hero, 1.2, {width: "100%"}, {width: "80%", ease: Power2.easeOut, delay: 1 })
+    // TweenMax.fromTo(slider, 1.2, {x: "-100%"}, {x: "0%", ease: Power2.easeOut, delay: 1 })
+    TweenMax.fromTo(logo, 0.5, {opacity: 0, x: 30}, {opacity: 1, x:0, ease: Power2.easeOut, delay: 1.7 })
+    TweenMax.fromTo(hamburger, 0.5, {opacity: 0, x: 30}, {opacity: 1, x:0, ease: Power2.easeOut, delay: 1.7 })
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <nav>
+          <h3 ref={el => (logo = el)} id="logo">volkswagen</h3>
+          <img ref={el => (hamburger = el)} style={{height: 25}} src={Menu} className="hamburger"></img>
+        </nav>
+        <section>
+          <div ref={el => (hero = el)} className="hero">
+            <img src={Bus}></img>
+            <h1 ref={el => (headline = el)} className="headline">Dream Big</h1>
+          </div>
+        </section>
       </header>
+
+      <div ref={el => (slider = el)} className="slider"></div>
     </div>
   );
 }
